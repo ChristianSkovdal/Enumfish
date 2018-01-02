cac4a81d0b8312437feb416a7e53759715c33dfa
drexcska <drexcska@A75105>
2018-01-02 18:47:43 +0100

    Updated to Extjs

--- EnumFish/Program.cs
+++ EnumFish/Program.cs
@@ -10,14 +10,16 @@ namespace EnumFish
     {
         static void Main(string[] args)
         {
-            if (args.Length != 2)
+            if (args.Length != 3)
             {
                 Error("Syntax Error - specify input and output filename");
                 return;
             }
 
+            string name = "";
             var infile = args[0];
             var outfile = args[1];
+            var ns = args[2];
             if (!File.Exists(infile))
             {
                 Error($"The file {infile} cannot be found");
@@ -38,8 +40,9 @@ namespace EnumFish
                     if (idxenumkw>=0 && toks.Length>=2 && !found)
                     {
                         found = true;
-                        var name = toks[idxenumkw+1];
-                        jslines.AppendLine($"const {name} = ");
+                        name = toks[idxenumkw+1];
+                        // Ext.define("Aux.Enum.SourceType",
+                        jslines.AppendLine($"Ext.define('{ns}.{name}',");
                     }
                     else if (found)
                     {
@@ -53,13 +56,18 @@ namespace EnumFish
                     }
                 }
 
-                var text = jslines.ToString();
-                if (string.IsNullOrEmpty(text))
+                if (string.IsNullOrEmpty(jslines.ToString()))
                 {
                     Console.WriteLine("Not a valid input file");
                 }
                 else
                 {
+                    jslines.AppendLine(");");
+                    // var SourceType = new Ext.create('Aux.Enum.SourceType');
+                    jslines.AppendLine($"var {name} = new Ext.create('{ns}.{name}');");
+
+                    var text = jslines.ToString();
+
                     File.WriteAllText(outfile, text);
                     Console.WriteLine($"Wrote the enum to file {outfile}");
                 }
